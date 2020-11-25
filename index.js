972612a37713e3901e864869b7fa82c4a26d8dec
Zesha <esershnr@gmail.com>
2020-11-25 12:19:50 +0300

    Menü Animasyonu eklendi

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const toTop = document.querySelector('#toTop');
+const menu = document.querySelector('#menu-container');
 
 window.addEventListener('scroll', () => {
-    if(window.pageYOffset > 100){
+    if(window.pageYOffset > 115){
         toTop.classList.add('active');
+        menu.classList.add('active');
     }else{
         toTop.classList.remove('active');
+        menu.classList.remove('active');
     }
 }) 
