d6ce602232169184d9ca0317da2daec295ef303d
Zesha <esershnr@gmail.com>
2021-01-14 23:37:48 +0300

    Biyografi Modal'ı Eklendi
    Animasyonla beraber

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ const ulMenu = document.querySelector('.navbar-ul');
 btnRes.addEventListener('click', () => {
     ulMenu.classList.toggle('responsive-m');
 });
+
+const btnModal = document.querySelector('.btn-1');
+
+const modal = document.querySelector('.modal-bg');
+
+const modalReal = document.querySelector('.modal-bio')
+
+const modalQuit = document.querySelector('.modal-kapat');
+
+
+btnModal.addEventListener('click', ()=>{
+    modal.classList.add('active');
+    modalReal.classList.add('active');
+})
+
+modalQuit.addEventListener('click', ()=>{
+    modal.classList.remove('active');
+    modalReal.classList.remove('active');
+})
