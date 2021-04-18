071f7291f408642dd2887b5bb5dd669305e8240d
Eser Şahiner <esershnr@gmail.com>
2021-04-18 19:43:13 +0300

    Bahar Güncellemesi

--- index.js
+++ index.js
@@ -44,3 +44,7 @@ modalQuit.addEventListener('click', ()=>{
     modal.classList.remove('active');
     modalReal.classList.remove('active');
 })
+
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
